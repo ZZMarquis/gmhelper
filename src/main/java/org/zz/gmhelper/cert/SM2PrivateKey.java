5f9c436140dc23a239781bde5fba3a4a361da837
ZZMarquis <zhonglingjian3821@163.com>
2019-03-13 18:02:10 +0800

    [MOD]格式化代码

--- src/main/java/org/zz/gmhelper/cert/SM2PrivateKey.java
+++ src/main/java/org/zz/gmhelper/cert/SM2PrivateKey.java
@@ -7,14 +7,12 @@ import org.bouncycastle.asn1.DERBitString;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
-import org.bouncycastle.asn1.x9.X962Parameters;
 import org.bouncycastle.asn1.x9.X9ObjectIdentifiers;
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
 import org.bouncycastle.jcajce.provider.asymmetric.util.ECUtil;
 import org.bouncycastle.jcajce.provider.config.ProviderConfiguration;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.zz.gmhelper.BCECUtil;
 
 import java.io.IOException;
 import java.security.spec.ECParameterSpec;
@@ -30,8 +28,7 @@ public class SM2PrivateKey extends BCECPrivateKey {
     }
 
     @Override
-    public void setPointFormat(String style)
-    {
+    public void setPointFormat(String style) {
         withCompression = !("UNCOMPRESSED".equalsIgnoreCase(style));
     }
 
@@ -42,56 +39,42 @@ public class SM2PrivateKey extends BCECPrivateKey {
      * @return a PKCS8 representation of the key.
      */
     @Override
-    public byte[] getEncoded()
-    {
+    public byte[] getEncoded() {
         ECParameterSpec ecSpec = getParams();
         ProviderConfiguration configuration = BouncyCastleProvider.CONFIGURATION;
         ASN1Encodable params = SM2PublicKey.ID_SM2_PUBKEY_PARAM;
 
         int orderBitLength;
-        if (ecSpec == null)
-        {
+        if (ecSpec == null) {
             orderBitLength = ECUtil.getOrderBitLength(configuration, null, this.getS());
-        }
-        else
-        {
+        } else {
             orderBitLength = ECUtil.getOrderBitLength(configuration, ecSpec.getOrder(), this.getS());
         }
 
         PrivateKeyInfo info;
-        org.bouncycastle.asn1.sec.ECPrivateKey            keyStructure;
+        org.bouncycastle.asn1.sec.ECPrivateKey keyStructure;
 
-        if (sm2PublicKey != null)
-        {
+        if (sm2PublicKey != null) {
             keyStructure = new org.bouncycastle.asn1.sec.ECPrivateKey(orderBitLength, this.getS(), sm2PublicKey, params);
-        }
-        else
-        {
+        } else {
             keyStructure = new org.bouncycastle.asn1.sec.ECPrivateKey(orderBitLength, this.getS(), params);
         }
 
-        try
-        {
+        try {
             info = new PrivateKeyInfo(new AlgorithmIdentifier(X9ObjectIdentifiers.id_ecPublicKey, params), keyStructure);
 
             return info.getEncoded(ASN1Encoding.DER);
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             return null;
         }
     }
 
-    private DERBitString getSM2PublicKeyDetails(SM2PublicKey pub)
-    {
-        try
-        {
+    private DERBitString getSM2PublicKeyDetails(SM2PublicKey pub) {
+        try {
             SubjectPublicKeyInfo info = SubjectPublicKeyInfo.getInstance(ASN1Primitive.fromByteArray(pub.getEncoded()));
 
             return info.getPublicKeyData();
-        }
-        catch (IOException e)
-        {   // should never happen
+        } catch (IOException e) {   // should never happen
             return null;
         }
     }
