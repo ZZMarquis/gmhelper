0418823bd165d12d1dd1c28853f74ff90207c8fc
ZZMarquis <445336534@qq.com>
2019-04-29 17:38:46 +0800

    [ADD]增加从PFX中解析出X509证书、公私钥的方法

--- src/main/java/org/zz/gmhelper/cert/SM2CertUtil.java
+++ src/main/java/org/zz/gmhelper/cert/SM2CertUtil.java
@@ -1,11 +1,23 @@
 package org.zz.gmhelper.cert;
 
+import org.bouncycastle.asn1.pkcs.ContentInfo;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
 import org.bouncycastle.jce.interfaces.ECPublicKey;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.jce.spec.ECParameterSpec;
 import org.bouncycastle.jce.spec.ECPublicKeySpec;
 import org.bouncycastle.math.ec.ECPoint;
+import org.bouncycastle.operator.InputDecryptorProvider;
+import org.bouncycastle.pkcs.PKCS12PfxPdu;
+import org.bouncycastle.pkcs.PKCS12SafeBag;
+import org.bouncycastle.pkcs.PKCS12SafeBagFactory;
+import org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfo;
+import org.bouncycastle.pkcs.jcajce.JcePKCSPBEInputDecryptorProviderBuilder;
+import org.zz.gmhelper.BCECUtil;
 import org.zz.gmhelper.SM2Util;
 
 import java.io.ByteArrayInputStream;
@@ -104,5 +116,55 @@ public class SM2CertUtil {
         NoSuchProviderException {
         CertificateFactory cf = CertificateFactory.getInstance("X.509", BouncyCastleProvider.PROVIDER_NAME);
         return cf.generateCertPath(certs);
+    }
+
+    public static X509Certificate getX509CertificateFromPfx(byte[] pfxDER, String passwd) throws Exception {
+        InputDecryptorProvider inputDecryptorProvider = new JcePKCSPBEInputDecryptorProviderBuilder()
+            .setProvider(BouncyCastleProvider.PROVIDER_NAME).build(passwd.toCharArray());
+        PKCS12PfxPdu pfx = new PKCS12PfxPdu(pfxDER);
+
+        ContentInfo[] infos = pfx.getContentInfos();
+        if (infos.length != 2) {
+            throw new Exception("Only support one pair ContentInfo");
+        }
+
+        for (int i = 0; i != infos.length; i++) {
+            if (infos[i].getContentType().equals(PKCSObjectIdentifiers.encryptedData)) {
+                PKCS12SafeBagFactory dataFact = new PKCS12SafeBagFactory(infos[i], inputDecryptorProvider);
+                PKCS12SafeBag[] bags = dataFact.getSafeBags();
+                X509CertificateHolder certHoler = (X509CertificateHolder) bags[0].getBagValue();
+                return SM2CertUtil.getX509Certificate(certHoler.getEncoded());
+            }
+        }
+
+        throw new Exception("Not found X509Certificate in this pfx");
+    }
+
+    public static BCECPublicKey getPublicKeyFromPfx(byte[] pfxDER, String passwd) throws Exception {
+        return SM2CertUtil.getBCECPublicKey(getX509CertificateFromPfx(pfxDER, passwd));
+    }
+
+    public static BCECPrivateKey getPrivateKeyFromPfx(byte[] pfxDER, String passwd) throws Exception {
+        InputDecryptorProvider inputDecryptorProvider = new JcePKCSPBEInputDecryptorProviderBuilder()
+            .setProvider(BouncyCastleProvider.PROVIDER_NAME).build(passwd.toCharArray());
+        PKCS12PfxPdu pfx = new PKCS12PfxPdu(pfxDER);
+
+        ContentInfo[] infos = pfx.getContentInfos();
+        if (infos.length != 2) {
+            throw new Exception("Only support one pair ContentInfo");
+        }
+
+        for (int i = 0; i != infos.length; i++) {
+            if (!infos[i].getContentType().equals(PKCSObjectIdentifiers.encryptedData)) {
+                PKCS12SafeBagFactory dataFact = new PKCS12SafeBagFactory(infos[i]);
+                PKCS12SafeBag[] bags = dataFact.getSafeBags();
+                PKCS8EncryptedPrivateKeyInfo encInfo = (PKCS8EncryptedPrivateKeyInfo) bags[0].getBagValue();
+                PrivateKeyInfo info = encInfo.decryptPrivateKeyInfo(inputDecryptorProvider);
+                BCECPrivateKey privateKey = BCECUtil.convertPKCS8ToECPrivateKey(info.getEncoded());
+                return privateKey;
+            }
+        }
+
+        throw new Exception("Not found Private Key in this pfx");
     }
 }
--- src/test/java/org/zz/gmhelper/cert/test/SM2PfxMakerTest.java
+++ src/test/java/org/zz/gmhelper/cert/test/SM2PfxMakerTest.java
@@ -3,6 +3,7 @@ package org.zz.gmhelper.cert.test;
 import org.bouncycastle.asn1.ASN1Encoding;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x509.KeyUsage;
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
@@ -12,6 +13,7 @@ import org.junit.Test;
 import org.zz.gmhelper.BCECUtil;
 import org.zz.gmhelper.SM2Util;
 import org.zz.gmhelper.cert.CommonUtil;
+import org.zz.gmhelper.cert.SM2CertUtil;
 import org.zz.gmhelper.cert.SM2PfxMaker;
 import org.zz.gmhelper.cert.SM2PublicKey;
 import org.zz.gmhelper.cert.SM2X509CertMaker;
@@ -26,6 +28,9 @@ public class SM2PfxMakerTest {
     static {
         Security.addProvider(new BouncyCastleProvider());
     }
+
+    private static final String TEST_PFX_PASSWD = "12345678";
+    private static final String TEST_PFX_FILENAME = "test.pfx";
 
     @Test
     public void testMakePfx() {
@@ -43,9 +48,31 @@ public class SM2PfxMakerTest {
             SM2PfxMaker pfxMaker = new SM2PfxMaker();
             PKCS10CertificationRequest request = new PKCS10CertificationRequest(csr);
             PublicKey subPub = BCECUtil.createPublicKeyFromSubjectPublicKeyInfo(request.getSubjectPublicKeyInfo());
-            PKCS12PfxPdu pfx = pfxMaker.makePfx(subKP.getPrivate(), subPub, cert, "12345678");
+            PKCS12PfxPdu pfx = pfxMaker.makePfx(subKP.getPrivate(), subPub, cert, TEST_PFX_PASSWD);
             byte[] pfxDER = pfx.getEncoded(ASN1Encoding.DER);
-            FileUtil.writeFile("D:/test.pfx", pfxDER);
+            FileUtil.writeFile(TEST_PFX_FILENAME, pfxDER);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testPfxSign() {
+        //先生成一个pfx
+        testMakePfx();
+
+        try {
+            byte[] pkcs12 = FileUtil.readFile(TEST_PFX_FILENAME);
+            BCECPublicKey publicKey = SM2CertUtil.getPublicKeyFromPfx(pkcs12, TEST_PFX_PASSWD);
+            BCECPrivateKey privateKey = SM2CertUtil.getPrivateKeyFromPfx(pkcs12, TEST_PFX_PASSWD);
+
+            String srcData = "1234567890123456789012345678901234567890";
+            byte[] sign = SM2Util.sign(privateKey, srcData.getBytes());
+            boolean flag = SM2Util.verify(publicKey, srcData.getBytes(), sign);
+            if (!flag) {
+                Assert.fail();
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
             Assert.fail();
