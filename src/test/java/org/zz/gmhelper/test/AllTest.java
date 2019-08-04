2986556783c927cf0a4c0a3a5585791e3a8924bd
ZZMarquis <445336534@qq.com>
2019-08-04 10:03:22 +0800

    [MOD]BC后续版本对国密算法实现又进行了一些修正和完善，升级BC版本到1.62

--- src/test/java/org/zz/gmhelper/test/AllTest.java
+++ src/test/java/org/zz/gmhelper/test/AllTest.java
@@ -3,8 +3,17 @@ package org.zz.gmhelper.test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.zz.gmhelper.cert.test.FileSNAllocatorTest;
+import org.zz.gmhelper.cert.test.SM2CertUtilTest;
+import org.zz.gmhelper.cert.test.SM2PfxMakerTest;
+import org.zz.gmhelper.cert.test.SM2PrivateKeyTest;
+import org.zz.gmhelper.cert.test.SM2X509CertMakerTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({SM2UtilTest.class, SM3UtilTest.class, SM4UtilTest.class, SM2KeyExchangeUtilTest.class})
+@SuiteClasses({BCECUtilTest.class, SM2UtilTest.class, SM3UtilTest.class, SM4UtilTest.class,
+    SM2KeyExchangeUtilTest.class, SM2PreprocessSignerTest.class,
+    // ------------------------------------
+    FileSNAllocatorTest.class, SM2CertUtilTest.class, SM2PfxMakerTest.class, SM2PrivateKeyTest.class,
+    SM2X509CertMakerTest.class})
 public class AllTest {
 }
